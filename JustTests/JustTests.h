74747b43bf8312710c917d9ae5948e703cda236d
Peter Tsoi <petertsoi@gmail.com>
2011-09-25 00:15:12 -0700

    Initial Commit

---
+++ JustTests/JustTests.h
@@ -0,0 +1,13 @@
+//
+//  JustTests.h
+//  JustTests
+//
+//  Created by Peter Tsoi on 9/25/11.
+//  Copyright 2011 Pixar Animation Studios. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JustTests : SenTestCase
+
+@end
